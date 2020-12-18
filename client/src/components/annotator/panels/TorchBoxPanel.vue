8d61adf47f7ee61cb972001ff83fd4c8eb54a702
alireza <alireza.elecat@gmail.com>
2020-12-18 20:16:40 +0100

    torchbox is added

---
+++ client/src/components/annotator/panels/TorchBoxPanel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-show="torchbox.isActive">
+    <PanelButton name="Delete TrochBox" @click="torchbox.deleteTorchbox" />
+    <PanelToggle name="Auto Select Color" v-model="torchbox.color.auto" />
+    <PanelToggle
+      v-show="torchbox.color.auto"
+      name="Only Black or White"
+      v-model="torchbox.color.blackOrWhite"
+    />
+    <PanelInputString
+      name="Stroke Color"
+      v-model="torchbox.polygon.pathOptions.strokeColor"
+    />
+  </div>
+</template>
+
+<script>
+import PanelButton from "@/components/PanelButton";
+import PanelToggle from "@/components/PanelToggle";
+import PanelInputString from "@/components/PanelInputString";
+import PanelInputNumber from "@/components/PanelInputNumber";
+
+export default {
+  name: "TrochBoxPanel",
+  components: { PanelButton, PanelToggle, PanelInputString, PanelInputNumber },
+  props: {
+    torchbox: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
